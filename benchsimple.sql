a3dce6b886f98327f9c6c0f69a24ffffbb458a9d
Peter Geoghegan <pg@heroku.com>
2014-01-07 15:52:31 -0800

    refactor

--- benchsimple.sql
+++ benchsimple.sql
@@ -2,4 +2,4 @@
 \setrandom rec 1 :extent
 \setrandom irec 1 :extent
 --set transaction_isolation = 'serializable';
-with rej as(insert into foo(a, b, c) values(:rec, :rec * random(), 'fd') on duplicate key lock for update returning rejects *) update foo set c = rej.c from rej where foo.a = rej.a;
+with rej as(insert into foo(merge, b, c) values(:rec, :rec * random(), 'fd') on duplicate key lock for update returning rejects *) update foo set c = rej.c from rej where foo.merge = rej.merge;
--- simple.sh
+++ simple.sh
@@ -2,16 +2,14 @@
 while true
 do
 rand=`shuf -i 10-20 -n 1`
-echo "trying $rand second run"
+echo "trying $0 $rand second run"
 psql -c "drop table if exists foo;"
 echo 'create unlogged table foo
 (
-a int4 primary key,
+merge int4 primary key,
 b int4,
 c text
 );' | psql
 pgbench -f benchsimple.sql -c 8 -T $rand -n -s 150000
-# don't trust index to indicate count of rows present/presence of spurious dups
-psql -c "set enable_indexscan = off; set enable_indexonlyscan = off; select a, count(*) from foo group by a having count(*) > 1 order by 1;"
-psql -c "set enable_indexscan = off; set enable_indexonlyscan = off; select count(*) as total_count from foo;"
+./foocount.sh
 done
