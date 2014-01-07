89f296af693317d1844728492260b3adb11e192a
Peter Geoghegan <pg@heroku.com>
2014-01-07 15:33:04 -0800

    comments

--- two.sh
+++ two.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# The point of this benchmark is in large part to get a sense of the
+# performance overhead of unnecessarily value locking a serial PK, where
+# presumably the sequential serial values represent a worse case for value
+# locking.  In the btree locking patch, we lock primary key last and release it
+# first, partly in anticipation of this problem.
 while true
 do
 rand=`shuf -i 10-20 -n 1`
@@ -11,7 +17,7 @@ uniqueval int4 unique,
 payload text
 );' | psql
 pgbench -f benchtwo.sql -c 8 -T $rand -n -s 150000
-# don't trust index to indicate number/Presence of spurious dups
+# don't trust index to indicate count of rows present/presence of spurious dups
 psql -c "set enable_indexscan = off; set enable_indexonlyscan = off; select a, count(*) from foo group by a having count(*) > 1 order by 1;"
 psql -c "set enable_indexscan = off; set enable_indexonlyscan = off; select count(*) as total_count from foo;"
 done
