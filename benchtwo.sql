3a9fbf0acee44e02577dd942069b2706473a0974
Peter Geoghegan <pg@heroku.com>
2014-01-07 16:27:27 -0800

    main

--- benchtwo.sql
+++ benchtwo.sql
@@ -1,5 +1,5 @@
 \set extent 10 * :scale
 \setrandom rec 1 :extent
 \setrandom irec 1 :extent
--- note that we just use serial value for PK a:
+-- Note that we implicitly just use sequence value for PK column a (a is of type SERIAL):
 with rej as(insert into foo(merge, payload) values(:rec, 'insert') on duplicate key lock for update returning rejects *) update foo set payload = 'update' from rej where foo.merge = rej.merge;
---
+++ main.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+./simple.sh			3
+./two.sh			3
+./smalltwo.sh		3
+./text.sh			3
