36d7e0ef088213cf1fcdd7ccf9e4c69b22e322c5
Peter Geoghegan <pg@heroku.com>
2014-01-07 17:03:08 -0800

    indicate tests passed

--- foocount.sh
+++ foocount.sh
@@ -4,5 +4,21 @@
 # where conflicts are expected; when done, it should have no duplicate values
 
 # don't trust index to indicate count of rows present/presence of spurious dups
-psql -c "set enable_indexscan = off; set enable_indexonlyscan = off; select merge, count(*) from foo group by merge having count(*) > 1 order by 1;"
+dupcount=`psql -c "set enable_indexscan = off; set enable_indexonlyscan = off; select merge, count(*) from foo group by merge having count(*) > 1 order by 1;"`
+
+red='\e[0;31m'
+green='\e[0;32m'
+NC='\e[0m' # No Color
+
 psql -c "set enable_indexscan = off; set enable_indexonlyscan = off; select count(*) as total_count from foo;"
+
+if [[ "$dupcount" == *"0 rows"* ]]
+then
+	echo -e "\n${green}Test passed:${NC}\n"
+	echo "$dupcount"
+	exit 0
+else
+	echo -e "\n${red}Test failed:${NC}\n"
+	echo "$dupcount"
+	exit 1
+fi
