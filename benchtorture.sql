f8ae4251760d228a4dce36351107e15aa63766db
Peter Geoghegan <pg@heroku.com>
2014-01-16 00:25:03 -0800

    Remove torture correlation

--- benchtorture.sql
+++ benchtorture.sql
@@ -7,4 +7,12 @@
 \setrandom recf 1 :extent
 \setrandom recg 1 :extent
 \setrandom recmerge 1 :extent
-with rej as(insert into foo(a, b, c, d, e, f, g, merge, payload) values (:reca, :recb, :recc, :recd, :rece, :recf, :recg, :recmerge, 'insert'), (:recb, :recf, :rece, :recg, :rece, :recb, :reca, :recmerge, 'insert') on duplicate key lock for update returning rejects ctid, *) update foo set payload = (random() * 5)::text from rej where foo.ctid = rej.ctid;
+\setrandom sreca 1 :extent
+\setrandom srecb 1 :extent
+\setrandom srecc 1 :extent
+\setrandom srecd 1 :extent
+\setrandom srece 1 :extent
+\setrandom srecf 1 :extent
+\setrandom srecg 1 :extent
+\setrandom srecmerge 1 :extent
+with rej as(insert into foo(a, b, c, d, e, f, g, merge, payload) values (:reca, :recb, :recc, :recd, :rece, :recf, :recg, :recmerge, 'insert'), (:sreca, :srecb, :srecc, :srecd, :srece, :srecf, :srecg, :srecmerge, 'insert') on duplicate key lock for update returning rejects ctid, *) update foo set payload = (random() * 5)::text from rej where foo.ctid = rej.ctid;
