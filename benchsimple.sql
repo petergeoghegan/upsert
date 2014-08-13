bfe8f5de069c06862336c1e187be947b247c1b31
Peter Geoghegan <pg@heroku.com>
2014-08-13 14:48:21 -0700

    Missed a spot

--- benchsimple.sql
+++ benchsimple.sql
@@ -1,4 +1,4 @@
 \set extent 10 * :scale
 \setrandom rec 1 :extent
 \setrandom irec 1 :extent
-with rej as(insert into foo(merge, b, c) values(:rec, :rec * random(), 'foofoofoo') on conflict select * for update) update foo set c = rej.c from rej where foo.merge = rej.merge;
+insert into foo(merge, b, c) values(:rec, :rec * random(), 'foofoofoo') on conflict update set c = :rec * random();
